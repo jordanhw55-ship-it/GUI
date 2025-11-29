5f58304a5c19e9a37ca1a36b5be4f4e5501ed07e
Jordan HW <jordanhw55@outlook.com>
2025-11-29 08:56:49 +0000

    Sync from Cloud Shell

--- ui_tab_widgets.py
+++ ui_tab_widgets.py
@@ -215,10 +215,9 @@ class QuickcastTab(QWidget):
         
         # --- Main Remapping Panel (Left) ---
         remap_panel = QWidget()
-        remap_layout = QVBoxLayout(remap_panel) # Changed to QVBoxLayout
-        remap_layout.addWidget(self.main_keys_group)
-        remap_layout.addWidget(self.numpad_keys_group)
-        remap_layout.addStretch() # Add stretch to push the boxes up
+        remap_layout = QHBoxLayout(remap_panel)
+        remap_layout.addWidget(self.main_keys_group, 2)
+        remap_layout.addWidget(self.numpad_keys_group, 1)
         main_layout.addWidget(remap_panel, 2) # Give it more space
 
         # --- Main Keys Grid ---
