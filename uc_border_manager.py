c275b723ece87a30b31a20da98b6723f4e58d9ee
Jordan HW <jordanhw55@outlook.com>
2025-12-03 19:47:17 +0000

    Sync from Cloud Shell

--- uc_border_manager.py
+++ uc_border_manager.py
@@ -27,7 +27,7 @@ class BorderManager:
             "Minimap Border": {
                 "target_tile": "humanuitile01",
                 "shape_type": "relative_rect",
-                "shape_data": [0.0371, 0.4297, 0.5391, 0.5391] # Precise values for the minimap cutout
+                "shape_data": [0.037109375, 0.4296875, 0.5390625, 0.5390625] # High-precision values for the minimap cutout
             },
             "Action Bar Top Edge": {
                 "target_tile": "humanuitile02",
