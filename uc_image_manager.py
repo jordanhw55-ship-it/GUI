94e9e5906a1e7d022aa12f29b5fa772b69042c6f
Jordan HW <jordanhw55@outlook.com>
2025-12-03 18:00:06 +0000

    Sync from Cloud Shell

--- uc_image_manager.py
+++ uc_image_manager.py
@@ -55,7 +55,7 @@ class ImageManager:
                 item_index = sum(1 for asset in self.dock_assets if not asset.is_border_asset) # Use manager's list
 
             items_per_row = 4
-            padding = 5
+            padding = 10
             asset_width = 128
             asset_height = 128
 
