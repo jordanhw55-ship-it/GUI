74a4fbffff918994f04a25891995832da3ac2e86
Jordan HW <jordanhw55@outlook.com>
2025-12-03 17:54:58 +0000

    Sync from Cloud Shell

--- uc_image_manager.py
+++ uc_image_manager.py
@@ -56,8 +56,8 @@ class ImageManager:
 
             items_per_row = 4
             padding = 5
-            asset_width = 68
-            asset_height = 68
+            asset_width = 120
+            asset_height = 120
 
             col = item_index % items_per_row
             row = item_index // items_per_row
