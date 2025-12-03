fa68fd0cef8b8e585e9e921d632748725cc30f41
Jordan HW <jordanhw55@outlook.com>
2025-12-03 14:05:38 +0000

    Sync from Cloud Shell

---
+++ uc_standalone.py
@@ -0,0 +1,11 @@
+import tkinter as tk
+from tkinter import messagebox
+from ui_creator import ImageEditorApp
+
+if __name__ == "__main__":
+    root = tk.Tk()
+    app = ImageEditorApp(root)
+    try:
+        root.mainloop()
+    except Exception as e:
+        messagebox.showerror("Application Error", f"An error occurred: {e}") # type: ignore
