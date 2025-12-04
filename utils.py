7af2d107e910066140f271bd6d68dd97815fc33c
Jordan HW <jordanhw55@outlook.com>
2025-12-04 00:18:30 +0000

    Sync from Cloud Shell

--- utils.py
+++ utils.py
@@ -1,6 +1,17 @@
 import sys
 import os
 
+def get_base_path():
+    """ Get absolute path to resource, works for dev and for PyInstaller/Nuitka """
+    if getattr(sys, 'frozen', False):
+        # If the application is run as a bundle, the base path is the directory
+        # where the executable is located.
+        return os.path.dirname(sys.executable)
+    else:
+        # The application is not frozen
+        # Change this to the directory of your main script
+        return os.path.dirname(os.path.abspath(__file__))
+
 DARK_STYLE = """
     /* Black/Orange Theme */
     QWidget {
@@ -228,14 +239,3 @@ OCEAN_STYLE = """
         image: url(none);
     }
 """
-
-def get_base_path():
-    """ Get absolute path to resource, works for dev and for PyInstaller/Nuitka """
-    if getattr(sys, 'frozen', False):
-        # If the application is run as a bundle, the base path is the directory
-        # where the executable is located.
-        return os.path.dirname(sys.executable)
-    else:
-        # The application is not frozen
-        # Change this to the directory of your main script
-        return os.path.dirname(os.path.abspath(__file__))
